a53fb50948de076e022bdd6ea5524c7f36c36df8
Satya Prakash Kaki <satya.kaki@incedoinc.com>
2022-09-14 12:35:19 -0400

    Adding the Test classes

---
+++ src/test/java/com/incedo/workflow/util/TestControllerExampleApplicationTests.java
@@ -0,0 +1,26 @@
+package com.incedo.workflow.util;
+
+import com.incedo.workflow.controller.HomeController;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestControllerExampleApplicationTests {
+
+	@Autowired
+	private HomeController homeController;
+
+	@Test
+	public void contextLoads() {
+		Assertions.assertThat(homeController).isNot(null);
+
+	}
+
+
+}
