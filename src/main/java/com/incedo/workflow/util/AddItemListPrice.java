6df1dd539e174f0b8986ba733790198cf1541a72
hari <hgshrestha23@gmail.com>
2022-09-16 12:37:35 -0500

    modified date issue

--- src/main/java/com/incedo/workflow/util/AddItemListPrice.java
+++ src/main/java/com/incedo/workflow/util/AddItemListPrice.java
@@ -1,5 +1,7 @@
 package com.incedo.workflow.util;
 
+import com.incedo.workflow.exception.BPMNErrorList;
+import com.incedo.workflow.exception.PriceNotFound;
 import com.incedo.workflow.model.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
@@ -16,15 +18,23 @@ public class AddItemListPrice implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        int itemPrice = (int) execution.getVariable("itemPrice");
-        Item eachItem = (Item) execution.getVariable("eachItemOrder");
-        List<Item> itemList = (ArrayList<Item>) execution.getVariable("itemList");
-        for (Item item : itemList) {
-            if (item.getItemId().equals(eachItem.getItemId())) {
-                item.setPrice(itemPrice);
+
+        try {
+            int itemPrice = (int) execution.getVariable("itemPrice");
+            Item eachItem = (Item) execution.getVariable("eachItemOrder");
+            List<Item> itemList = (ArrayList<Item>) execution.getVariable("itemList");
+            for (Item item : itemList) {
+                if (item.getItemId().equals(eachItem.getItemId())) {
+                    item.setPrice(itemPrice);
+                }
             }
+            log.info(String.valueOf(itemList));
+            execution.setVariable("itemList", itemList);
+        } catch (Exception ex) {
+            log.error("Price not Found.");
+            throw  new PriceNotFound(BPMNErrorList.ERROR_PRICE_NOT_FOUND, "Price not found in dmn table.");
         }
-        log.info(String.valueOf(itemList));
-        execution.setVariable("itemList", itemList);
+
+
     }
 }
